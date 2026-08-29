1a9eca65b7f094b56619f1f021609385f0b41a67
JoaoPedro <jp.rvboas@outlook.com>
2026-08-28 21:25:37 -0300

    correção atividade 01

--- atividade01/conversor.js
+++ atividade01/conversor.js
@@ -8,14 +8,14 @@
 
 function conversorUSD(a){
     let dolar = 5.14;
-    let resultadoUSD = a * dolar; 
+    let resultadoUSD = a / dolar; 
     
     return + resultadoUSD.toFixed(2); 
 }
 
 function conversorEUR(b) {
     let euro = 6.01;
-    let resultadoEUR = b*euro;
+    let resultadoEUR = b / euro;
     return + resultadoEUR.toFixed(2);
     
 }
